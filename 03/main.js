7ed10cc320859dd11e52d4064516970e4f3aa909
유단비 <dbeksql94@gmail.com>
2022-01-06 14:37:29 +0900

    소수 구하기 삽질

--- 03/main.js
+++ 03/main.js
@@ -1,6 +1,22 @@
-for(let x=1; x<=135; x++){
-  if(135 % x == 0){
-    y = 135 / x;
-    console.log(y);
-  }  
+// 소수 구하기 
+// 소수 : 1과 그 수 자신 이외의 자연수로는 나눌 수 없는, 1보다 큰 자연수..
+
+let result = prompt('입력하슈','');
+let decimal = [];
+
+for(let x=1; x<=result; x++){
+  for(let y=1; y<=x; y++){
+    if(x*y == result){
+      decimal.push(x)
+      decimal.push(y)
+      decimal.push('/')
+      console.log(decimal)
+    }
+  }
+}
+
+if(decimal.length == 2){
+  console.log('소수입니다.')
+} else {
+  console.log('소수가 아닙니다.')
 }
